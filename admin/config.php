d0ea6b11c8685b04a1bb30d122e59efadb9e2c4a
Asadullah Al Galib <galib.bcse@gmail.com>
2020-07-16 23:12:11 +0600

    First Release

---
+++ admin/config.php
@@ -0,0 +1,18 @@
+<?php
+$servername      = "localhost";
+$username        = "root";
+$password        = "admin";
+$dbname          = "polling";
+$site_url_suffix = "polling";
+
+try {
+
+    $conn = new PDO("mysql:host=$servername;dbname=" . $dbname, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+
+    echo "<h2>Connection failed: " . $e->getMessage() . "</h2>";
+    die();
+
+}
---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<?php include './includes/header.php';?>
+
+<div class="main-container mt-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3>Dashboard</h3>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <form action="index.php">
+                    <input type="text">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include './includes/footer.php';?>
